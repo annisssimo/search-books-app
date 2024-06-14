c03631500866bcc4694acfec2af55b54be58e7eb
annisssimo <anyalar04@gmail.com>
2024-06-14 18:44:21 +0300

    fix: combined all changes into one commit
    (cherry picked from commit 4b7678e743e6fe5216688f80138a7a72776ce950)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0);
-  let thid;
+  const [count, setCount] = useState(0)
   return (
     <>
       <div>
@@ -26,10 +25,10 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Vite and React logos to learn
       </p>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
