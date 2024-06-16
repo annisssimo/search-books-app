790d6e1472c56043bdb74ae0f3ba29e849ef6d31
annisssimo <anyalar04@gmail.com>
2024-06-16 18:26:18 +0300

    feat: drop-down list

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,13 @@
 import './Header.css';
 import SearchBar from '../SearchBar/SearchBar.jsx';
+import DropDown from '../DropDown/DropDown.jsx';
 
 function Header() {
   return (
     <header>
       <h1>Search for books</h1>
       <SearchBar />
+      <DropDown />
     </header>
   );
 }
