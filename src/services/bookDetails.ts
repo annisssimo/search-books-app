a2d1fff188302357a6bdc82d62ddb7ddd8dbd46d
annisssimo <anyalar04@gmail.com>
2024-06-26 14:47:58 +0300

    refactor: api key and url are constants in bookDetails

--- src/services/bookDetails.ts
+++ src/services/bookDetails.ts
@@ -1,11 +1,9 @@
-// import { GOOGLE_BOOKS_API_URL } from '../constants/apiConstants';
+import { GOOGLE_BOOKS_API_URL } from '../constants/apiConstants';
 
-// const VITE_GOOGLE_BOOKS_API_KEY = import.meta.env.VITE_GOOGLE_BOOKS_API_KEY;
+const VITE_GOOGLE_BOOKS_API_KEY = import.meta.env.VITE_GOOGLE_BOOKS_API_KEY;
 
 export const fetchBookDetails = async (id: string) => {
-  //   const url = `${GOOGLE_BOOKS_API_URL}?q=${query}${categoryFilter}&orderBy=${sort}&startIndex=${startIndex}&maxResults=${maxResults}&key=${VITE_GOOGLE_BOOKS_API_KEY}`;
-
-  const url = `https://www.googleapis.com/books/v1/volumes/${id}?key=AIzaSyCHN4uhbCS9XCX1n3m-uPb8LYIAy5cCES0`;
+  const url = `${GOOGLE_BOOKS_API_URL}/${id}?key=${VITE_GOOGLE_BOOKS_API_KEY}`;
   const response = await fetch(url);
 
   if (!response.ok) {
