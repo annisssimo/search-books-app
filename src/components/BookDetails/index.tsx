25ba34ec7af1d74be999279fe181e6e3bcfb7205
annisssimo <anyalar04@gmail.com>
2024-07-01 13:53:28 +0300

    feat: add loading indicator on book details page

--- src/components/BookDetails/index.tsx
+++ src/components/BookDetails/index.tsx
@@ -3,48 +3,52 @@ import './index.css';
 import { useParams } from 'react-router-dom';
 
 import useBookDetails from '../../hooks/useBookDetails';
+import Spinner from '../Spinner';
 
 const BookDetails: React.FC = () => {
   const { id } = useParams<{ id: string }>();
   const { book, loading, error } = useBookDetails(id);
 
-  if (loading) return <div className="loading">Loading...</div>;
-  if (error) return <div className="error-message">{error}</div>;
-  if (!book) return <div className="error-message">No book details available</div>;
-
   const getImageLink = () => {
+    if (!book || !book.volumeInfo || !book.volumeInfo.imageLinks) return;
     const { thumbnail, smallThumbnail } = book.volumeInfo.imageLinks;
-    if (!thumbnail && !smallThumbnail) return;
     return thumbnail || smallThumbnail;
   };
 
   const renderDescription = () => {
-    if (book.volumeInfo.description) {
-      const tempElement = document.createElement('div');
-      tempElement.innerHTML = book.volumeInfo.description;
-      return tempElement.innerText;
+    if (!book || !book.volumeInfo || !book.volumeInfo.description) {
+      return 'No description available';
     }
-    return 'No description available';
+    const tempElement = document.createElement('div');
+    tempElement.innerHTML = book.volumeInfo.description;
+    return tempElement.innerText;
   };
 
   return (
     <div className="book-details-page">
-      <div className="book-details-image-container">
-        <img
-          className="book-details-image"
-          src={getImageLink()}
-          alt={book.volumeInfo.title}
-          onLoad={() => console.log('Image loaded')}
-        />
-      </div>
-      <div className="book-info">
-        {book.volumeInfo.categories && (
-          <div className="book-details-categories">{book.volumeInfo.categories.join(', ')}</div>
-        )}
-        <div className="book-details-title">{book.volumeInfo.title}</div>
-        <div className="book-details-authors">{book.volumeInfo.authors?.join(', ')}</div>
-        <div className="book-details-description">{renderDescription()}</div>
-      </div>
+      {loading && <Spinner />}
+      {error && <div className="error-message">{error}</div>}
+      {!loading && !book && <div className="error-message">No book details available</div>}
+      {book && (
+        <>
+          <div className="book-details-image-container">
+            <img
+              className="book-details-image"
+              src={getImageLink()}
+              alt={book.volumeInfo.title}
+              onLoad={() => console.log('Image loaded')}
+            />
+          </div>
+          <div className="book-info">
+            {book.volumeInfo.categories && (
+              <div className="book-details-categories">{book.volumeInfo.categories.join(', ')}</div>
+            )}
+            <div className="book-details-title">{book.volumeInfo.title}</div>
+            <div className="book-details-authors">{book.volumeInfo.authors?.join(', ')}</div>
+            <div className="book-details-description">{renderDescription()}</div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
--- src/components/Spinner/index.tsx
+++ src/components/Spinner/index.tsx
@@ -1,12 +1,9 @@
 import './index.css';
 
-import React from 'react';
-
 const Spinner: React.FC = () => {
   return (
     <div className="spinner">
-      <div className="double-bounce1"></div>
-      <div className="double-bounce2"></div>
+      <div className="circular"></div>
     </div>
   );
 };
